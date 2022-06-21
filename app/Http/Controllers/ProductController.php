3065fc790e49f935c413aca2f75d45b5b2f3d4e0
KamilWojtalak <kamilwojtalak99@gmail.com>
2022-06-22 01:33:57 +0200

    REST API with Access tokens

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+        $request->validated();
+
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Product::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, $id)
+    {
+        /** Get one product */
+        $product = Product::findOrFail($id);
+
+        /** Validation */
+        $request->validated();
+
+        /** Update product */
+        return $product->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Product::destroy($id);
+    }
+    
+    /**
+     * searchByName
+     *
+     * @param  string $name
+     * @return \Illuminate\Http\Response
+     */
+    public function searchByName(string $name)
+    {
+        return Product::where('name', $name);
+    }
+}
